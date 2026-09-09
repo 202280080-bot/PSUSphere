a278ee5777e66be6f712d5b6a28ed79c69f649a3
MaryMae <202280080@psu.palawan.edu.ph>
2026-09-09 18:12:50 +0800

    Update admin configuration and requirements

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -2,20 +2,56 @@ from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
+# Task A – CollegeAdmin
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
 
 
+# Task B – ProgramAdmin
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+
+# Task C – OrganizationAdmin
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
+
+
+# Student Admin
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
-    search_fields = ("lastname", "firstname", "student_id")
+    list_display = (
+        "student_id",
+        "lastname",
+        "firstname",
+        "middlename",
+        "program",
+    )
+    search_fields = (
+        "lastname",
+        "firstname",
+        "student_id",
+    )
 
 
+# Organization Membership Admin
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined")
+    list_display = (
+        "student",
+        "get_member_program",
+        "organization",
+        "date_joined",
+    )
     search_fields = (
         "student__lastname",
         "student__firstname",
@@ -24,20 +60,3 @@ class OrgMemberAdmin(admin.ModelAdmin):
 
     def get_member_program(self, obj):
         return obj.student.program
-
-
-# Example Organizations:
-# 1. ACS
-# 2. SITE
-#
-# Example Student 1:
-# Student ID: 202280080
-# Last Name: Apilan
-# First Name: Mary Mae
-# Middle Name: M.
-#
-# Example Student 2:
-# Student ID: 202330198
-# Last Name: Bendanillo
-# First Name: Irene
-# Middle Name: C.
